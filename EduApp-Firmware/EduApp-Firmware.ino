9d570cc86d19c87a60bcd9f8a79a13a741dd3a8f
Adrian Korzeniowski <adikorzeniowski@gmail.com>
2018-08-13 22:55:49 +0200

    Basic implementation of serial reading

--- EduApp-Firmware/EduApp-Firmware.ino
+++ EduApp-Firmware/EduApp-Firmware.ino
@@ -3,10 +3,28 @@ void setup() {
 }
 
 void loop() {
-  processSerialData()
+  processSerialData();
 }
 
 void processSerialData() {
-  
+  char temp;
+  String data = "";
+  unsigned long timeout = millis();
+  while(millis() - timeout <= 100) {
+    while(Serial.available()) {
+      temp = Serial.read();
+      timeout = millis();
+      if(temp == '\r' || temp == '\n') continue;
+      data += temp;
+    }
+  }
+ 
+  if(data.length() == 0) return;
+ 
+  data.toUpperCase();
+ 
+  Serial.print("GOT: \"");
+  Serial.print(data);
+  Serial.println("\"");
 }
 
