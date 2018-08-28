7ecaff9507e2fd58461d7428bda6793d1b8258ac
Kamil Krupa <kamilkrupa96@gmail.com>
2018-08-28 23:37:02 +0200

    Implement components initialization and communication with Android

--- EduApp-Firmware/EduApp-Firmware.ino
+++ EduApp-Firmware/EduApp-Firmware.ino
@@ -1,4 +1,21 @@
+const int PIN_BUZZER1 = 2;
+const int PIN_LED1 = 13;
+const int PIN_LED2 = 12;
+const int PIN_RGB1_R = 9;
+const int PIN_RGB1_G = 10;
+const int PIN_RGB1_B = 11;
+const int PIN_LED4 = 8;
+const int PIN_LED5 = 7;
+const int PIN_BTN1 = 4;
+const int PIN_BTN2 = 5;
+const int PIN_BTN3 = 3;
+const int PIN_BTN4 = 6;
+const int PIN_POT1 = A1;
+const int PIN_TEMP1 = A2;
+const int PIN_LIGHT1 = A3;
+
 const int MAX_COMMAND_SIZE = 64;
+const int MAX_RESPONSE_SIZE = 256;
 const int TIMEOUT = 100;
 const int NUMBER_BASE = 10;
 const char* COMPONENT_SEPARATOR = ":";
@@ -7,8 +24,31 @@ const char* COMMAND_TERMINATOR = ";";
 
 int commandCount = 0;
 
+unsigned int rgbRedValue, rgbGreenValue, rgbBlueValue;
+
+typedef struct {
+  const char* name;
+  int pin;
+  int componentType;
+} Component;
+
 void setup() {
   Serial.begin(9600);
+  pinMode(PIN_BUZZER1, OUTPUT);
+  pinMode(PIN_LED1, OUTPUT);
+  pinMode(PIN_LED2, OUTPUT);
+  pinMode(PIN_RGB1_R, OUTPUT);
+  pinMode(PIN_RGB1_G, OUTPUT);
+  pinMode(PIN_RGB1_B, OUTPUT);
+  pinMode(PIN_LED4, OUTPUT);
+  pinMode(PIN_LED5, OUTPUT);
+  pinMode(PIN_BTN1, INPUT);
+  pinMode(PIN_BTN2, INPUT);
+  pinMode(PIN_BTN3, INPUT);
+  pinMode(PIN_BTN4, INPUT);
+  pinMode(PIN_POT1, INPUT);
+  pinMode(PIN_TEMP1, INPUT);
+  pinMode(PIN_LIGHT1, INPUT);
 }
 
 void loop() {
@@ -86,6 +126,146 @@ void processSerialData() {
   int commandID = strtol(commandIDString, &conversionError, NUMBER_BASE);
   if (*conversionError) return;
 
+  
+
+  if(strcmp(component, "arduino1") == 0) {
+    if(strcmp(type, "get") == 0) {
+      printGetComponentsResponse(component, commandID);
+    }
+  } else if(strcmp(componentType, "led") == 0) {
+    int componentPin;
+    switch(componentID) {
+      case 1: componentPin = PIN_LED1; break;
+      case 2: componentPin = PIN_LED2; break;
+      case 4: componentPin = PIN_LED4; break;
+      case 5: componentPin = PIN_LED5; break;
+      
+      default: return;
+    }
+    if(strcmp(type, "get") == 0) {
+      if(strcmp(action, "value") == 0) {
+        printGetValueResponse(component, commandID, componentPin);
+      } else if(strcmp(action, "pin") == 0) {
+        printGetPinResponse(component, commandID, componentPin);
+      } else {
+        printGetValueResponse(component, commandID, componentPin);
+        printGetPinResponse(component, commandID, componentPin);
+      }
+    } else if(strcmp(type, "set") == 0) {
+      int newComponentValue = atoi(action);
+      digitalWrite(componentPin, newComponentValue != 0);
+      printGetValueResponse(component, commandID, componentPin);
+    }
+  } else if(strcmp(componentType, "rgb") == 0) {
+    if(strcmp(type, "get") == 0) {
+      if(strcmp(action, "value") == 0) {
+        printGetRGBValueResponse(component, commandID, PIN_RGB1_R, PIN_RGB1_G, PIN_RGB1_B);
+      } else if(strcmp(action, "pin") == 0) {
+        printGetPinResponse(component, commandID, "x");
+      } else {
+        printGetRGBValueResponse(component, commandID, PIN_RGB1_R, PIN_RGB1_G, PIN_RGB1_B);
+        printGetPinResponse(component, commandID, "x");
+      }
+    } else if(strcmp(type, "set") == 0) {
+      unsigned long newComponentValue = strtol(action, &conversionError, NUMBER_BASE);
+      if (*conversionError) return;
+      rgbRedValue = (newComponentValue >> 16) & 0xFF;
+      rgbGreenValue = (newComponentValue >> 8) & 0xFF;
+      rgbBlueValue = newComponentValue & 0xFF;
+      analogWrite(PIN_RGB1_R, rgbRedValue);
+      analogWrite(PIN_RGB1_G, rgbGreenValue);
+      analogWrite(PIN_RGB1_B, rgbBlueValue);
+      printGetRGBValueResponse(component, commandID, PIN_RGB1_R, PIN_RGB1_G, PIN_RGB1_B);
+    }
+  } else if(strcmp(componentType, "buz") == 0) {
+    int componentPin;
+    switch(componentID) {
+      case 1: componentPin = PIN_BUZZER1; break;
+      default: return;
+    }
+    if(strcmp(type, "get") == 0) {
+      if(strcmp(action, "value") == 0) {
+        printGetValueResponse(component, commandID, componentPin);
+      } else if(strcmp(action, "pin") == 0) {
+        printGetPinResponse(component, commandID, componentPin);
+      } else {
+        printGetValueResponse(component, commandID, componentPin);
+        printGetPinResponse(component, commandID, componentPin);
+      }
+    } else if(strcmp(type, "set") == 0) {
+      int newComponentValue = atoi(action);
+      digitalWrite(componentPin, newComponentValue != 0);
+      printGetValueResponse(component, commandID, componentPin);
+    }
+  } else if(strcmp(componentType, "btn") == 0) {
+    int componentPin;
+    switch(componentID) {
+      case 1: componentPin = PIN_BTN1; break;
+      case 2: componentPin = PIN_BTN2; break;
+      case 3: componentPin = PIN_BTN3; break;
+      case 4: componentPin = PIN_BTN4; break;
+      default: return;
+    }
+    if(strcmp(type, "get") == 0) {
+      if(strcmp(action, "value") == 0) {
+        printGetValueResponse(component, commandID, componentPin);
+      } else if(strcmp(action, "pin") == 0) {
+        printGetPinResponse(component, commandID, componentPin);
+      } else {
+        printGetValueResponse(component, commandID, componentPin);
+        printGetPinResponse(component, commandID, componentPin);
+      }
+    }
+  } else if(strcmp(componentType, "temp") == 0) {
+    int componentPin;
+    switch(componentID) {
+      case 1: componentPin = PIN_TEMP1; break;
+      default: return;
+    }
+    if(strcmp(type, "get") == 0) {
+      if(strcmp(action, "value") == 0) {
+        printGetAnalogValueResponse(component, commandID, componentPin);
+      } else if(strcmp(action, "pin") == 0) {
+        printGetPinResponse(component, commandID, componentPin);
+      } else {
+        printGetAnalogValueResponse(component, commandID, componentPin);
+        printGetPinResponse(component, commandID, componentPin);
+      }
+    }
+  } else if(strcmp(componentType, "light") == 0) {
+    int componentPin;
+    switch(componentID) {
+      case 1: componentPin = PIN_LIGHT1; break;
+      default: return;
+    }
+    if(strcmp(type, "get") == 0) {
+      if(strcmp(action, "value") == 0) {
+        printGetAnalogValueResponse(component, commandID, componentPin);
+      } else if(strcmp(action, "pin") == 0) {
+        printGetPinResponse(component, commandID, componentPin);
+      } else {
+        printGetAnalogValueResponse(component, commandID, componentPin);
+        printGetPinResponse(component, commandID, componentPin);
+      }
+    }
+  } else if(strcmp(componentType, "pot") == 0) {
+    int componentPin;
+    switch(componentID) {
+      case 1: componentPin = PIN_POT1; break;
+      default: return;
+    }
+    if(strcmp(type, "get") == 0) {
+      if(strcmp(action, "value") == 0) {
+        printGetAnalogValueResponse(component, commandID, componentPin);
+      } else if(strcmp(action, "pin") == 0) {
+        printGetPinResponse(component, commandID, componentPin);
+      } else {
+        printGetAnalogValueResponse(component, commandID, componentPin);
+        printGetPinResponse(component, commandID, componentPin);
+      }
+    }
+  }
+  
   Serial.print("Component: \"");
   Serial.print(component);
   Serial.print("\", component type: \"");
@@ -107,5 +287,44 @@ void processSerialData() {
     Serial.print("\", ");
   }
   Serial.println("}");
+}
+
+void printGetComponentsResponse(char* component, int commandID) {
+  char response[MAX_RESPONSE_SIZE] = "";
+  snprintf(response, MAX_RESPONSE_SIZE, "response:%s,%d,led1,led2,led4,led5,rgb1,buz1,btn1,btn2,btn3,btn4,pot1,temp1,light1;", component, commandID);
+  Serial.println(response);
+}
+
+void printGetValueResponse(char* component, int commandID, int pin) {
+  char response[MAX_RESPONSE_SIZE] = "";
+  int componentValue = digitalRead(pin);
+  snprintf(response, MAX_RESPONSE_SIZE, "response:%s,%d,raw,%d;", component, commandID, componentValue);
+  Serial.println(response);
+}
+
+void printGetAnalogValueResponse(char* component, int commandID, int pin) {
+  char response[MAX_RESPONSE_SIZE] = "";
+  int componentValue = analogRead(pin);
+  snprintf(response, MAX_RESPONSE_SIZE, "response:%s,%d,raw,%d;", component, commandID, componentValue);
+  Serial.println(response);
+}
+
+void printGetRGBValueResponse(char* component, int commandID, int pinR, int pinG, int pinB) {
+  char response[MAX_RESPONSE_SIZE] = "";
+  unsigned long componentValue = (rgbRedValue << 16) | (rgbGreenValue << 8) | rgbBlueValue;
+  snprintf(response, MAX_RESPONSE_SIZE, "response:%s,%d,raw,%d;", component, commandID, componentValue);
+  Serial.println(response);
+}
+
+void printGetPinResponse(char* component, int commandID, int pin) {
+  char response[MAX_RESPONSE_SIZE] = "";
+  snprintf(response, MAX_RESPONSE_SIZE, "response:%s,%d,pin,%d;", component, commandID, pin);
+  Serial.println(response);
+}
+
+void printGetPinResponse(char* component, int commandID, char* pin) {
+  char response[MAX_RESPONSE_SIZE] = "";
+  snprintf(response, MAX_RESPONSE_SIZE, "response:%s,%d,pin,%s;", component, commandID, pin);
+  Serial.println(response);
 }
 
