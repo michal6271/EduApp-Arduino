1f9a8cfe096fdbf8b7f59d47e418a4c093b439e7
Kamil Krupa <kamilkrupa96@gmail.com>
2018-09-15 02:15:58 +0200

    Remove redundant data

--- EduApp-Firmware/EduApp-Firmware.ino
+++ EduApp-Firmware/EduApp-Firmware.ino
@@ -6,10 +6,6 @@ const int JSON_BUFFER_CAPACITY = JSON_OBJECT_SIZE(16);
 const int MAX_COMMAND_SIZE = 256;
 const int TIMEOUT = 100;
 
-typedef enum CommandType {
-  CMD_GET, CMD_SET
-};
-
 const BooleanLedComponent booleanLeds[] = {
   BooleanLedComponent(13),
 
@@ -58,12 +54,13 @@ void loop() {
 void processJsonCommand(JsonObject& command) {
   StaticJsonBuffer<JSON_BUFFER_CAPACITY> JSONBuffer;
   const char* componentName = command["componentName"];
-  const CommandType commandType = (CommandType) command.get<int>("commandType");
+  const bool setValue = command.containsKey("data");
   const int componentId = command["componentId"];
   const int commandId = command["commandId"];
   
 
   JsonObject& response = JSONBuffer.createObject();
+  JsonObject& responseData = JSONBuffer.createObject();
   response["commandId"] = commandId;
   response["componentName"] = componentName;
   response["componentId"] = componentId;
@@ -72,33 +69,25 @@ void processJsonCommand(JsonObject& command) {
   if(checkComponent(componentName, "BooleanLedComponent", componentId, booleanLedsCount)) {
     BooleanLedComponent booleanLedComponent = booleanLeds[componentId];
     response["componentAddr"] = booleanLedComponent.getAddress();
-    switch(commandType) {
-      case CMD_SET:
-        booleanLedComponent.setState(command["data"]["state"]);
-      case CMD_GET:
-        JsonObject& data = JSONBuffer.createObject();
-        data["state"] = booleanLedComponent.getState();
-        response["data"] = data;
-        response["isError"] = false;
-        break;
+    if(setValue) {
+      booleanLedComponent.setState(command["data"]["state"]);
     }
+    responseData["state"] = booleanLedComponent.getState();
+    response["data"] = responseData;
+    response["isError"] = false;
   } else if(checkComponent(componentName, "PwmRgbLedComponent", componentId, pwmRgbLedsCount)) {
     PwmRgbLedComponent pwmRgbLedComponent = pwmRgbLeds[componentId];
     response["componentAddr"] = pwmRgbLedComponent.getAddress();
-    switch(commandType) {
-      case CMD_SET:
-        pwmRgbLedComponent.setRed(command["data"]["red"]);
-        pwmRgbLedComponent.setGreen(command["data"]["green"]);
-        pwmRgbLedComponent.setBlue(command["data"]["blue"]);
-      case CMD_GET:
-        JsonObject& data = JSONBuffer.createObject();
-        data["red"] = pwmRgbLedComponent.getRed();
-        data["green"] = pwmRgbLedComponent.getGreen();
-        data["blue"] = pwmRgbLedComponent.getBlue();
-        response["data"] = data;
-        response["isError"] = false;
-        break;
+    if(setValue) {
+      pwmRgbLedComponent.setRed(command["data"]["red"]);
+      pwmRgbLedComponent.setGreen(command["data"]["green"]);
+      pwmRgbLedComponent.setBlue(command["data"]["blue"]);
     }
+    responseData["red"] = pwmRgbLedComponent.getRed();
+    responseData["green"] = pwmRgbLedComponent.getGreen();
+    responseData["blue"] = pwmRgbLedComponent.getBlue();
+    response["data"] = responseData;
+    response["isError"] = false;
   }
   response.prettyPrintTo(Serial);
 }
