5337f85852fe6e8b2a60ca6218f4578c95655747
Kamil Krupa <kamilkrupa96@gmail.com>
2018-09-15 03:51:08 +0200

    Implement WS2812 rgb leds handling

--- EduApp-Firmware/EduApp-Firmware.ino
+++ EduApp-Firmware/EduApp-Firmware.ino
@@ -1,10 +1,13 @@
 #include <ArduinoJson.h>
 #include "BooleanLedComponent.cpp"
 #include "PwmRgbLedComponent.cpp"
+#include "WS2812RgbLedComponent.cpp"
 
 const int JSON_BUFFER_CAPACITY = JSON_OBJECT_SIZE(16);
 const int MAX_COMMAND_SIZE = 256;
 const int TIMEOUT = 100;
+
+Adafruit_NeoPixel ws2812Driver = Adafruit_NeoPixel(5, 12, NEO_GRB + NEO_KHZ800);
 
 BooleanLedComponent booleanLeds[] = {
   BooleanLedComponent(13),
@@ -20,6 +23,15 @@ PwmRgbLedComponent pwmRgbLeds[] = {
   PwmRgbLedComponent(9, 10, 11)
 };
 const int pwmRgbLedsCount = sizeof(pwmRgbLeds)/sizeof(PwmRgbLedComponent);
+
+WS2812RgbLedComponent ws2812RgbLeds[] = {
+  WS2812RgbLedComponent(&ws2812Driver, 0),
+  WS2812RgbLedComponent(&ws2812Driver, 1),
+  WS2812RgbLedComponent(&ws2812Driver, 2),
+  WS2812RgbLedComponent(&ws2812Driver, 3),
+  WS2812RgbLedComponent(&ws2812Driver, 4)
+};
+const int ws2812RgbLedsCount = sizeof(ws2812RgbLeds)/sizeof(WS2812RgbLedComponent);
 
 void setup() {
   Serial.begin(9600);
@@ -86,6 +98,20 @@ void processJsonCommand(JsonObject& command) {
     responseData["red"] = pwmRgbLedComponent.getRed();
     responseData["green"] = pwmRgbLedComponent.getGreen();
     responseData["blue"] = pwmRgbLedComponent.getBlue();
+    response["data"] = responseData;
+    response["isError"] = false;
+  } else if(checkComponent(componentName, "WS2812RgbLedComponent", componentId, ws2812RgbLedsCount)) {
+    WS2812RgbLedComponent& ws2812RgbLedComponent = ws2812RgbLeds[componentId];
+    response["componentAddr"] = ws2812RgbLedComponent.getAddress();
+    if(setValue) {
+      ws2812RgbLedComponent.setRed(command["data"]["red"]);
+      ws2812RgbLedComponent.setGreen(command["data"]["green"]);
+      ws2812RgbLedComponent.setBlue(command["data"]["blue"]);
+      ws2812RgbLedComponent.update();
+    }
+    responseData["red"] = ws2812RgbLedComponent.getRed();
+    responseData["green"] = ws2812RgbLedComponent.getGreen();
+    responseData["blue"] = ws2812RgbLedComponent.getBlue();
     response["data"] = responseData;
     response["isError"] = false;
   }
